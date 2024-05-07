75db60306a17e50813af8adbdb8cca53e445c67f
Shubham Dhobakle <97725662+shubhamdhobale@users.noreply.github.com>
2024-05-07 10:39:29 +0530

    file upload using cloudinary and multer

--- project/src/index.js
+++ project/src/index.js
@@ -1,6 +1,6 @@
 // import mongoose from 'mongoose'
-import express from 'express';
 // import DB_NAME from './constant'
+import express from 'express';
 import dotenv from 'dotenv'
 import connectDB from './db/index.js';
 

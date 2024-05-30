26764384bad0b7c8e672ed02db89fe1a5ee9895d
shubhamdhobale2021 <shubhamdhobale2021.it@mmcoe.edu.in>
2024-05-30 07:30:44 +0530

    subscription model

--- project/src/models/subscription.models.js
+++ project/src/models/subscription.models.js
@@ -9,6 +9,6 @@ const subscriptionSchema = new mongoose.Schema({
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User'
   }
-})
+}, {timestamps: true})
 
-export const Subscription = new mongoose.model("Subscription" , subscriptionSchema)
+export const Subscription = mongoose.model("Subscription" , subscriptionSchema)
